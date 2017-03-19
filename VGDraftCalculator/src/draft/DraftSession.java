f5b396e2f966ab37f88d8e96540482bc814d6d9d
ksscott <kennethscott@kennethsmacbookpro>
2017-03-19 17:55:46 -0500

    remove unused imports

--- VGDraftCalculator/src/draft/DraftSession.java
+++ VGDraftCalculator/src/draft/DraftSession.java
@@ -1,11 +1,8 @@
 package draft;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class DraftSession implements Cloneable {
 
